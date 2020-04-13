19df639684ca6892ffb90cfa1514f19a9b437c98
sbakii <baki61@gmail.com>
2020-04-14 00:43:54 +0300

    1404

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,43 @@
 import Layout from '../components/layout'
 import Head from 'next/head'
-import unfetch from 'isomorphic-unfetch'
 import Link from 'next/link'
-function HomePage({characters}) {
-    return(
-         <Layout>
+import unfetch from 'isomorphic-unfetch'
+import slug from 'slug'
 
-        <Head>
-            <title>Ana Sayfa</title>
-        </Head>
+function HomePage({ characters }) {
+  return (
+    <Layout>
+      <Head>
+        <title>Ana sayfa</title>
+      </Head>
+      <h1 className="title">THE RICK AND MORTY</h1>
 
-        <h1> RICK AND MORTY CARACTERS.</h1>
+      <ul>
+        {characters.results.map(character => (
+          <li key={character.id}>
+            <Link
+              href="/character/[slug]"
+              as={`/character/${slug(character.name)}-${character.id}`}
+            >
+              <a>{character.name}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
 
-       <ul>
-       {characters.results.map(character => (
-             <li key={character.id}>
-                 <Link href ="/character/[id]" as={'/character/${character.id}'}>
-                 <a> {character.name}  </a>
-                 </Link> </li>))}
-        
-       </ul>
+      <style jsx>{``}</style>
     </Layout>
-    )
+  )
 }
 
 export async function getStaticProps() {
-    //datafetch
-    const data = await unfetch("https://rickandmortyapi.com/api/character/")
-    const characters = await data.json()
-  
-    return {
-      props: {
-          characters 
-      }, 
+  const data = await unfetch('https://rickandmortyapi.com/api/character/')
+  const characters = await data.json()
+  return {
+    props: {
+      characters
     }
   }
+}
+
 export default HomePage
